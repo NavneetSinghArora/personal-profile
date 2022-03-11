080f8cc98f417d53f9ccdba6b7ee08b3edcaa80f
NavneetSinghArora <31856757+NavneetSinghArora@users.noreply.github.com>
2022-03-11 15:46:48 +0100

    Fix for Navigation bar Toggle Functionality

---
+++ scripts/common.js
@@ -0,0 +1,39 @@
+$(document).on('click', function (e) {
+     if ($("div").hasClass("navbar-collapse collapse show") == true) {
+        $('.navbar-collapse').collapse('hide');
+     }
+ });
+$(document).ready(function () {
+    $("button#toggle_button_overlap.navbar-toggler.btn-sm").hide()
+})
+
+$(document).click(function(event) {
+    var text = $(event.target).text();
+    console.log(event.target.readyState)
+});
+
+function toggle() {
+    $("button#toggle_button_overlap.navbar-toggler.btn-sm").show()
+    $("button#toggle_button.navbar-toggler.btn-sm").hide()
+
+    let element = document.querySelector("button#toggle_button_overlap.navbar-toggler.btn-sm");
+    let hidden = element.getAttribute("hidden");
+
+    if (hidden) {
+       element.removeAttribute("hidden");
+    }
+
+}
+
+function retoggle() {
+    $('.navbar-collapse').collapse('hide');
+    $("button#toggle_button.navbar-toggler.btn-sm").show()
+    $("button#toggle_button_overlap.navbar-toggler.btn-sm").hide()
+
+    let element = document.querySelector("button#toggle_button_overlap.navbar-toggler.btn-sm");
+    let hidden = element.getAttribute("hidden");
+
+    if (!hidden) {
+       element.setAttribute("hidden", "hidden");
+    }
+}
